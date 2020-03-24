557242d84911fe976a758d43de38f5a8e461d127
yairslo7 <60138113+yairslo7@users.noreply.github.com>
2020-03-24 20:18:09 +0200

    Add files via upload

---
+++ c#project1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter length of array: ");
+
+            //length
+            int lengthoafrray = Convert.ToInt32(Console.ReadLine());
+
+            //multply cells.
+            Console.WriteLine("enter number to multiply: ");
+            int numtomul = Convert.ToInt32(Console.ReadLine());
+
+            //array
+            int[] arr = new int[lengthoafrray];
+
+            Console.WriteLine($"num of elemnts:{lengthoafrray}");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * numtomul;
+                Console.WriteLine(arr[i] + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
